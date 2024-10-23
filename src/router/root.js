8bc11d48b96b51e966667bcfa7826ddc133d5e25
Shingeunsu <shings9239@naver.com>
2024-10-23 16:54:30 +0900

    탭관리

--- src/router/root.js
+++ src/router/root.js
@@ -3,15 +3,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../login/login";
 import Main from "../view/00_Common/main/main";
 import Layout from "../view/00_Common/layout/layout"
+import Empty from "../components/layout/header/empty"; // Empty 컴포넌트 import 추가
 
 import SaleRouter from "./saleRouter";
 import OrderRouter from "./orderRouter";
 import MoveRouter from "./moveRouter";
 import StockRouter from "./stockRouter";
 import CustRouter from "./custRouter";
-
-
-
 
 const Root = createBrowserRouter([
     {
@@ -25,6 +23,10 @@ const Root = createBrowserRouter([
             {
                 path: 'main',
                 element: <Main />
+            },
+            {
+                path: 'empty', 
+                element: <Empty />
             },
             {
                 path: 'sale',
